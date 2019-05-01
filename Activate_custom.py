e620644c1f9f6603cd1c9a5bd1c254e61c680fc7
David Brownell <db@DavidBrownell.com>
2019-04-30 22:05:31 -0700

    Updated activation output.

--- Activate_custom.py
+++ Activate_custom.py
@@ -85,7 +85,6 @@ def GetCustomActions(
                         version=version,
                     ),
                 ),
-                CurrentShell.Commands.Message(""),
             ]
 
         # Set the compiler
